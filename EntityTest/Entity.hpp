e7e47c024339c208918e0a2670e540808231f10f
Markus Lobedann <markus.lobedann@imat-uve.de>
2018-05-17 17:31:02 +0200

    can only be created through CScene now

--- EntityTest/Entity.hpp
+++ EntityTest/Entity.hpp
@@ -13,11 +13,6 @@ public:
 		m_id { 0 }
 	{}
 
-	// TODO allow creation only from CScene
-	Entity( const std::uint32_t id ) :
-		m_id { id }
-	{}
-
 	const std::uint32_t &Id() const
 	{
 		return( m_id );
@@ -29,6 +24,10 @@ public:
 	}
 
 private:
+	Entity( const std::uint32_t id ) :
+		m_id { id }
+	{}
+
 	size_t m_id;
 	std::uint32_t m_version;
 };
