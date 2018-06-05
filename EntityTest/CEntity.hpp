b488f15854cb360d7174bfd15a7c8a92e2ac40fc
Markus Lobedann <markus.lobedann@imat-uve.de>
2018-06-05 16:07:34 +0200

    dismantle CEntity

--- EntityTest/CEntity.hpp
+++ EntityTest/CEntity.hpp
@@ -1,33 +1,19 @@
 #ifndef CENTITY_HPP
 #define CENTITY_HPP
 
-#include <string>
-#include <memory>
-#include <array>
-
-#include "CLogger.hpp"
-
-#include "CTransformComponent.hpp"
 #include "CBoundingBox.hpp"
 
 
 class CEntity final
 {
 public:
-	explicit CEntity( const std::string &name );
-	~CEntity();
+	CEntity()
+	{};
 
-	const std::string &Name( void ) const;
+	~CEntity()
+	{};
 
-	const std::uint32_t Id = ++s_lastId;
-
-	CTransformComponent Transform;
 	CBoundingBox BoundingBox;
-
-private:
-	const std::string m_name;
-
-	static std::uint32_t s_lastId;
 };
 
 #endif // COBJECT_HPP
