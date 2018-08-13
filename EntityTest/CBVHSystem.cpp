5bf5a3786f8db65ef41de775719c0fe7545015b2
Markus Lobedann <markus.lobedann@gmail.com>
2018-08-14 01:10:22 +0200

    delete entities which leave the octree

--- EntityTest/CBVHSystem.cpp
+++ EntityTest/CBVHSystem.cpp
@@ -6,19 +6,34 @@
 
 void CBVHSystem::Process()
 {
+	CLogger::Log( "\tprocessing: CBVHSystem" );
+	
 	const auto start = std::chrono::system_clock::now();
 
 	m_octree.Clear();
+	
+	std::vector< CEntity > entitiesForDeletion;
 
-	m_ecs.ForEach<CTransform>( [ this ] ( const auto &transformEntity, const auto transformComponent )
+	m_ecs.ForEach<CTransform>( [ this, &entitiesForDeletion ] ( const auto &transformEntity, const auto transformComponent )
 	{
 		auto const boundingBox = m_ecs.GetComponent< CBoundingBox >( transformEntity );
 
 		if( !m_octree.Add( transformEntity, *transformComponent, boundingBox ) )
 		{
+			entitiesForDeletion.push_back( transformEntity );
 			CLogger::Log( "entity '" + std::to_string( transformEntity.Id() ) + "' lies outside of the OCTree: " + glm::to_string( transformComponent->Position ) );
 		}
 	} );
+	
+	for( const auto &entity : entitiesForDeletion )
+	{
+		m_ecs.Destroy( entity );
+	}
+
+	if( entitiesForDeletion.size() > 0 )
+	{
+		CLogger::Log( "destroyed " + std::to_string( entitiesForDeletion.size() ) + " entities which dropped out of the world" );
+	}
 
 	const auto end = std::chrono::system_clock::now();
 	const std::chrono::duration<double> diff = end - start;
