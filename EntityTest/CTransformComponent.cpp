ee4a868f727e5ea6ee788fbf22e19a18b2bb04b4
Markus Lobedann <markus.lobedann@gmail.com>
2018-11-02 09:49:02 +0100

    convert class CWorld to just the namespace "World"

--- EntityTest/CTransformComponent.cpp
+++ EntityTest/CTransformComponent.cpp
@@ -3,13 +3,13 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtx/quaternion.hpp>
 
-#include "CWorld.hpp"
+#include "World.hpp"
 
 void CTransformComponent::Rotate( const float pitchAngle, const float yawAngle, const float rollAngle )
 {
-	Orientation = glm::angleAxis( glm::radians( pitchAngle ), CWorld::X ) * Orientation;
-	Orientation = glm::angleAxis( glm::radians( yawAngle ), CWorld::Y ) * Orientation;
-	Orientation = glm::angleAxis( glm::radians( rollAngle ), CWorld::Z ) * Orientation;
+	Orientation = glm::angleAxis( glm::radians( pitchAngle ), World::X ) * Orientation;
+	Orientation = glm::angleAxis( glm::radians( yawAngle ), World::Y ) * Orientation;
+	Orientation = glm::angleAxis( glm::radians( rollAngle ), World::Z ) * Orientation;
 }
 
 const glm::mat4 CTransformComponent::ViewMatrix( void ) const
---
+++ EntityTest/World.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+namespace World
+{
+	static const glm::vec3 X { 1.0f, 0.0f, 0.0f };
+	static const glm::vec3 Y { 0.0f, 1.0f, 0.0f };
+	static const glm::vec3 Z { 0.0f, 0.0f, 1.0f };
+};
